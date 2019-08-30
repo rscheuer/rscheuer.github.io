2ee86ea5a0613db7c12858715c72ab6bc85f909f
Ryan Scheuer <ryan.scheuer1@gmail.com>
2019-08-30 06:21:04 -0400

    updated for presentation

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+		$('a').click(function(event) {
+//		event.preventDefault();
+//		newLocation = $(this).attr("href");
+		$('body').fadeOut(200, newpage);
+	});
+	function newpage() {
+//		window.location = newLocation;
+	}
+});
