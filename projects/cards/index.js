adb33dd0b92fb555b4b0198bf7ce792951e54640
Ryan Scheuer <ryan.scheuer1@gmail.com>
2021-02-09 17:08:22 -0500

    oof

---
+++ projects/cards/index.js
@@ -0,0 +1,33 @@
+// var els = document.getElementsByClassName("stack__card");
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+	return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+$(window).scroll(function () {
+	// Setting: Start fading halfway up the page
+	var startPos = 0.2;
+
+	// Cache window object
+	var $w = $(window);
+
+	// Basically, we go through each element and check its relative position within the viewport
+	$(".stack__card").each(function () {
+		// Get current relative position in viewport, based on the top edge
+		var pos = $(this).offset().top - $w.scrollTop();
+
+		// Get viewport height
+		var vh = $w.height();
+        
+        // console.log(pos);
+        var s = scale(pos, 0, 3000, 0.3, 1);
+        // console.log(s);
+		if (pos < vh * startPos) {
+			// If element has past the starting threshold, we fade it
+			$(this).css("transform", "scale(" + scale(pos, 0, 3000, 1, 1.3) + ")");
+			// console.log(pos);
+		} else {
+			$(this).css("transform", "scale(" + scale(pos, 0, 3000, 1, 1.3) + ")");
+		}
+	});
+});
