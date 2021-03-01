ed9f9f4eed42b31cc12f41c0b0037673461e3c72
Ryan Scheuer <ryan.scheuer1@gmail.com>
2021-03-01 12:29:03 -0500

    groupfont

--- projects/cards/index.js
+++ projects/cards/index.js
@@ -5,25 +5,25 @@ const scale = (num, in_min, in_max, out_min, out_max) => {
 };
 
 $(window).scroll(function () {
-	// Setting: Start fading halfway up the page
+	// Setting: Start shrinking halfway up the page
 	var startPos = 0.2;
 
 	// Cache window object
 	var $w = $(window);
 
-	// Basically, we go through each element and check its relative position within the viewport
+	// Basically, go through each element and check its relative position within the viewport
 	$(".stack__card").each(function () {
 		// Get current relative position in viewport, based on the top edge
 		var pos = $(this).offset().top - $w.scrollTop();
 
-		// Get viewport height
+		// viewport height
 		var vh = $w.height();
         
         // console.log(pos);
         var s = scale(pos, 0, 3000, 0.3, 1);
         // console.log(s);
 		if (pos < vh * startPos) {
-			// If element has past the starting threshold, we fade it
+			// If element has past the starting threshold, scale
 			$(this).css("transform", "scale(" + scale(pos, 0, 2000, 1, 1.1) + ")");
 			// console.log(pos);
 		} else {
