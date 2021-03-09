14b7c8661d53e44461642379ab63a865fa317a08
Ryan Scheuer <ryan.scheuer1@gmail.com>
2021-03-09 12:44:51 -0500

    added loco-scroll

--- projects/mark/js/main.js
+++ projects/mark/js/main.js
@@ -1,21 +1,8 @@
+const scroller = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
+});
 
   window.onload = function () {
-    Marquee3k.init()
-
-    lax.init()
-
-    // Add a driver that we use to control our animations
-    lax.addDriver('scrollY', function () {
-      return window.scrollY
-    })
-
-    // Add animation bindings to elements
-    lax.addElements('sauce', {
-      scrollY: {
-        scaleY: [
-          ["elInY", "elOutY"],
-          [10, 1],
-        ]
-      }
-    })
+    Marquee3k.init();
   }
