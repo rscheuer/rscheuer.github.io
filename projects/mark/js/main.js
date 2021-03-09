c4cb9ce345678149ab27dd0e93eda281aed1f770
Ryan Scheuer <ryan.scheuer1@gmail.com>
2021-03-09 13:01:12 -0500

    added lax

--- projects/mark/js/main.js
+++ projects/mark/js/main.js
@@ -1,8 +1,26 @@
-const scroller = new LocomotiveScroll({
-  el: document.querySelector('[data-scroll-container]'),
-  smooth: true
-});
+// const scroller = new LocomotiveScroll({
+//   el: document.querySelector('[data-scroll-container]'),
+//   smooth: true
+// });
 
   window.onload = function () {
     Marquee3k.init();
+
+    lax.init()
+
+    // Add a driver that we use to control our animations
+    lax.addDriver('scrollY', function () {
+      return window.scrollY
+    })
+
+    // Add animation bindings to elements
+    lax.addElements('.selector', {
+      scrollY: {
+        translateY: [
+          ["elInY", "elCenterY", "elOutY"],
+          [0, '-screenHeight/10', '-screenHeight/4'],
+        ]
+      }
+    })
   }
+
